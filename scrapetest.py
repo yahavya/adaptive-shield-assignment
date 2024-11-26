48141bf2c8de2b0c13b3edfb0aa1502acf3e9f04
Ron <ronnieyahav@gmail.com>
2024-11-26 10:30:48 +0200

    Added basic refactoring and documentation

--- scrapetest.py
+++ scrapetest.py
@@ -1,4 +1,3 @@
-import csv
 from bs4 import BeautifulSoup
 import requests
 
@@ -24,80 +23,109 @@ def get_html_content(url):
 
 
 """
-Function for writing data to a specified filename in CSV format.
+This function receives a list of collateral adjectives and returns a set of unique adjectives.
 """
 
 
-def write_to_file(data, filename):
-    with open(filename, "w", newline="") as file:
-        writer = csv.writer(file)
-        # writer.writerow(["Name", "Origin", "Classification", "Family"])
-        writer.writerows(data)
-
-
-tableData = get_html_content(url)
-delimiter = "###"
-
-
-for row in tableData:
-    for space in row.findAll("br"):
-        space.replaceWith(delimiter)
-
-
-tableData = [[td.text for td in row.find_all("td")] for row in tableData]
-
-animal_names = [row[0] for row in tableData if len(row) > 0]  # Get animal names
-collateral_adj = [
-    row[5] for row in tableData if len(row) > 0
-]  # Get collateral adjectives
-
-animal_names_with_collateral_adj = (
-    [  # Create tuples of (animal name, collateral adjective)
-        (name, col) for name, col in zip(animal_names, collateral_adj)
-    ]
-)
-
-collateral_adj_split = [adj.split("###") for adj in collateral_adj]
-
-
-def get_collective_adjectives_set(collateral_adj_split):
+def parse_collective_adjectives(collateral_adj_split):
     adj_joined_list = []
     for lst in collateral_adj_split:
         adj_joined_list.extend(lst)
 
-    for j in range(len(adj_joined_list)):
+    for j in range(
+        len(adj_joined_list)
+    ):  # Iterate over every collateral adjective in the list, removing suffix of [83], which is citation. This is a quick fix for preventing duplicates.
         try:
             i = adj_joined_list[j].index("[")
             adj_joined_list[j] = adj_joined_list[j][:i]
-            # print("found item", adj_joined_list[j], "with substring")
-        except ValueError as ve:
+        except (
+            ValueError
+        ) as ve:  # If the index is not found, it means the adjective does not contain "[", and we don't care, just continue
             continue
 
-    adj_joined_set = set(adj_joined_list)
+    adj_joined_set = set(adj_joined_list)  # Convert list to a set to remove duplicates
     adj_joined_set.remove("")  # Get rid of the "" key because it spams the results
+
     return adj_joined_set
 
 
-def is_exactly(s):
+"""
+This function receives the table data and returns a list of lists
+"""
 
-    return False
+
+def get_data_from_table(tableData):
+
+    delimiter = "###"  # Add a delimiter to separate collateral adjectives
+    for row in tableData:
+        for space in row.findAll("br"):
+            space.replaceWith(
+                delimiter
+            )  # Replace <br> tags with delimiter for easier parsing, not necessarily the best way to go, but easy and gets the job done
+    tableData = [
+        [td.text for td in row.find_all("td")] for row in tableData
+    ]  # Get data from table in lists format
+
+    return tableData
 
 
-adj_set = get_collective_adjectives_set(
+"""
+This function is used for getting animals and their collateral adjectives, 
+returns a list of tuples (animal name, collateral adjective), 
+along with the collateral adjectives, and the animal names, for comfortable use
+"""
+
+
+def get_animals_and_collateral_adjectives():
+
+    animal_names = [
+        row[0] for row in parsedTable if len(row) > 0
+    ]  # Get animal names, filtering out empty entries
+    collateral_adj = [
+        row[5] for row in parsedTable if len(row) > 0
+    ]  # Get collateral adjectives, filtering out empty entries
+
+    animal_names_with_collateral_adj = (
+        [  # Create tuples of (animal name, collateral adjective)
+            (name, col) for name, col in zip(animal_names, collateral_adj)
+        ]
+    )
+    return animal_names_with_collateral_adj, collateral_adj, animal_names
+
+
+tableData = get_html_content(
+    url
+)  # Use bs4 to parse the HTML content and return a table
+
+parsedTable = get_data_from_table(tableData)  # Extracting the data from the table
+
+animal_names_with_collateral_adj, collateral_adj, animal_names = (
+    get_animals_and_collateral_adjectives()
+)  # Get the animals and their collateral adjectives
+
+collateral_adj_split = [
+    adj.split("###") for adj in collateral_adj
+]  # This is the first step in parsing the collateral adjectives
+
+col_adj_set = parse_collective_adjectives(
     collateral_adj_split
-)  # Send the collective for parsing, returning only unique and clean adjectives
+)  # Send the collective adjectives for parsing, returning only real and unique adjectives in a set ("remove -, '', etc.")
 
 adjectives_with_animals = (
     dict()
 )  # Initialize the result dictionary which will map adjectives to their proper animals
 
-for key in adj_set:  # Iterate over every collective adjective that we have
+for (
+    key
+) in col_adj_set:  # Iterate over every collective adjective that we have in the set
     for (
         animal
     ) in (
         animal_names_with_collateral_adj
     ):  # Iterate over each animal in the (animal, adjectives) tuple
-        adjectives_list = animal[1].split("###")
+        adjectives_list = animal[1].split(
+            "###"
+        )  # Break down the adjectives to avoid duplication and incorrectly adding substrings instead of the exact adjectives
         if (
             key in adjectives_list
         ):  # Check if the adjective is in one of the animal's adjectives
@@ -111,4 +139,5 @@ for key in adj_set:  # Iterate over every collective adjective that we have
                     animal[0]
                 )  # Otherwise, add the animal to the proper (key,value) pair.
 
-print(adjectives_with_animals)
+
+print(adjectives_with_animals)  # Outputting the result dictionary
